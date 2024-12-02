df2edcf16e922136910152b3377e6b8b88b40b07
VadimZakirov <appolon17@gmail.com>
2024-12-02 23:59:57 +0500

    Start ces07

---
+++ src/test/java/ru/vadim/webfluxcourse/tests/sec07/AbstractWebClient.java
@@ -0,0 +1,25 @@
+package ru.vadim.webfluxcourse.tests.sec07;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.function.Consumer;
+
+@Slf4j
+public class AbstractWebClient {
+
+    protected <T>Consumer<T> print() {
+        return item -> log.info("received: {}", item);
+    }
+
+    protected WebClient createWebClient() {
+        return createWebClient(b -> {});
+    }
+
+    protected WebClient createWebClient(Consumer<WebClient.Builder> consumer) {
+        var builder = WebClient.builder()
+                .baseUrl("http://localhost:7080/demo02");
+        consumer.accept(builder);
+        return builder.build();
+    }
+}
